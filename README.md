2e3da8e024e30ed1e18d4b36b95cfa39ae0cbbdf
Morten Jagd Christensen <morten.christensen@esss.se>
2020-03-23 09:23:19 +0100

    added files

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # multicastrxtx
 Examples of multicast transmitter and reciever
+
+## build
+    > mkdir build
+    > cd build
+    > cmake ..
+    > make
+
+## run receiver(s)
+The receiver(s) listen for a line on the specified multicast
+address and udp port. Then prints the line.
+
+    > ./mcrecv multicast_ip udp_port
+    > ./mcrecv 224.2.3.4 9000
+
+## run transmitter
+The transmitter reads a line from the console and sends this on the
+specified multicast address and udp port
+
+    > ./mcsend multicast_ip udp_port
+    > ./mcsend 224.2.3.4 9000
---
+++ mcreceive.cpp
@@ -0,0 +1,100 @@
+#include <sys/types.h>          /* for type definitions */
+#include <sys/socket.h>         /* for socket API calls */
+#include <netinet/in.h>         /* for address structs */
+#include <arpa/inet.h>          /* for sockaddr_in */
+#include <stdio.h>              /* for printf() and fprintf() */
+#include <stdlib.h>             /* for atoi() */
+#include <string.h>             /* for strlen() */
+#include <unistd.h>             /* for close() */
+#include <sys/timeb.h>
+#include <stdlib.h>             /* for system() */
+
+#define MAX_LEN  1400           /* maximum receive string size */
+
+int rxpackets;
+int rxbytes;
+
+int main(int argc, char *argv[])
+{
+    int             sock;               /* socket descriptor */
+    int             flag_on = 1;        /* socket option flag */
+    struct sockaddr_in mc_addr;         /* socket address structure */
+    char            recv_str[MAX_LEN + 1];      /* buffer to receive string */
+    int             recv_len;           /* length of string received */
+    struct ip_mreq  mc_req;             /* multicast request structure */
+    char           *mc_addr_str;        /* multicast IP address */
+    unsigned short  mc_port;            /* multicast port */
+    struct sockaddr_in from_addr;       /* packet source */
+    unsigned int    from_len;           /* source addr length */
+
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: %s <Multicast IP> <Multicast Port>\n", argv[0]);
+        exit(1);
+    }
+
+    mc_addr_str = argv[1];      /* arg 1: multicast ip address */
+    mc_port = atoi(argv[2]);    /* arg 2: multicast port number */
+
+
+    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+    {
+        perror("socket() failed");
+        exit(1);
+    }
+
+    if ((setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &flag_on, sizeof(flag_on))) < 0)
+    {
+        perror("setsockopt(SO_REUSEADDR) failed");
+        exit(1);
+    }
+
+    if ((setsockopt(sock, SOL_SOCKET, SO_REUSEPORT, &flag_on, sizeof(flag_on))) < 0)
+    {
+        perror("setsockopt(SO_REUSEPORT) failed");
+        exit(1);
+    }
+
+    /* construct a multicast address structure */
+    memset(&mc_addr, 0, sizeof(mc_addr));
+    mc_addr.sin_family = AF_INET;
+    mc_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    mc_addr.sin_port = htons(mc_port);
+
+    /* bind to multicast address to socket */
+    if ((bind(sock, (struct sockaddr *)&mc_addr, sizeof(mc_addr))) < 0)
+    {
+        perror("bind() failed");
+        exit(1);
+    }
+
+    /* construct an IGMP join request structure */
+    mc_req.imr_multiaddr.s_addr = inet_addr(mc_addr_str);
+    mc_req.imr_interface.s_addr = htonl(INADDR_ANY);
+
+    /* send an ADD MEMBERSHIP message via setsockopt */
+    if ((setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, (void *)&mc_req, sizeof(mc_req))) < 0)
+    {
+        perror("setsockopt(IP_ADD_MEMBERSHIP) failed");
+        exit(1);
+    }
+
+    for (;;)
+    {
+        memset(recv_str, 0, sizeof(recv_str));
+        from_len = sizeof(from_addr);
+        memset(&from_addr, 0, from_len);
+
+        if ((recv_len = recvfrom(sock, recv_str, MAX_LEN, 0, (struct sockaddr *)&from_addr, &from_len)) < 0)
+        {
+            perror("recvfrom() failed");
+            exit(1);
+        }
+
+        rxbytes += strlen(recv_str);
+        rxpackets++;
+
+        printf("Packet %d (%d bytes): %s\n", rxpackets, recv_len, recv_str);
+        system(recv_str);
+    }
+}
