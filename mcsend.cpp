b4a3163e3fa7b07474eb2e963a77c4e1a33498e7
Morten Jagd Christensen <morten.christensen@esss.se>
2020-03-23 09:46:16 +0100

    some c++ features and comments

--- mcsend.cpp
+++ mcsend.cpp
@@ -1,65 +1,44 @@
-/********************************************************************
- Copyright (C) 2011 Morten Jagd Christensen
- all rights reserved
-********************************************************************/
+//*******************************************************************
+// Copyright (C) 2011-2020 Morten Jagd Christensen
+//*******************************************************************
 
-#include <sys/types.h>          /* for type definitions          */
-#include <sys/socket.h>         /* for socket API function calls */
-#include <netinet/in.h>         /* for address structs           */
-#include <arpa/inet.h>          /* for sockaddr_in               */
-#include <stdio.h>              /* for printf()                  */
-#include <stdlib.h>             /* for atoi()                    */
-#include <string.h>             /* for strlen()                  */
-#include <unistd.h>             /* for close()                   */
+#include <sys/types.h>          // for type definitions
+#include <sys/socket.h>         // for socket API function calls
+#include <netinet/in.h>         // for address structs
+#include <arpa/inet.h>          // for sockaddr_in
+#include <stdio.h>              // for printf()
+#include <stdlib.h>             // for atoi()
+#include <string.h>             // for strlen()
+#include <unistd.h>             // for close()
 
-#define MAX_LEN  1400           /* maximum string size to send   */
-
-#ifdef LWEFORMAT
-    #define LWEOFS 6
-#else 
-    #define LWEOFS 0
-#endif
-
-
-void hexdump(char * a, int len)
-{
-    for (int i=0; i<len; i++)
-    {
-        if ((i % 16) ==0)
-            printf("\n%04d: ", i);
-        printf("%02x ", a[i]);        
-    }
-}
+const int MaxLen{1400};         // maximum string size to send
 
 int main(int argc, char *argv[])
 {
-    int             sock;               /* socket descriptor            */
-    char            send_str[MAX_LEN];  /* string to send               */
-    struct sockaddr_in mc_addr;         /* socket address structure     */
-    unsigned int    send_len;           /* length of string to send     */
-    char           *mc_addr_str;        /* multicast IP address         */
-    unsigned short  mc_port;            /* multicast port               */
-    unsigned char   mc_ttl = 1;         /* time to live (hop count)     */
+    int             sock;               // socket descriptor
+    char            send_str[MaxLen];   // string to send
+    struct sockaddr_in mc_addr;         // socket address structure
+    unsigned int    send_len;           // length of string to send
+    char           *mc_addr_str;        // multicast IP address
+    unsigned short  mc_port;            // multicast port
+    unsigned char   mc_ttl{1};          // time to live (hop count)
 
 
-    if (argc != 3) 
-    {
+    if (argc != 3) {
         fprintf(stderr, "Usage: %s <Multicast IP> <Multicast Port>\n", argv[0]);
         exit(1);
     }
 
-    mc_addr_str = argv[1];              /* arg 1: multicast IP address  */
-    mc_port = atoi(argv[2]);            /* arg 2: multicast port number */
+    mc_addr_str = argv[1];              // arg 1: multicast IP address
+    mc_port = atoi(argv[2]);            // arg 2: multicast port number
 
-    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
-    {
+    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
         perror("socket() failed");
         exit(1);
     }
 
-    if ((setsockopt(sock, IPPROTO_IP, IP_MULTICAST_TTL, (void *)&mc_ttl, sizeof(mc_ttl))) < 0)
-    {
-        perror("setsockopt() MULTICAST TTL failed");
+    if ((setsockopt(sock, IPPROTO_IP, IP_MULTICAST_TTL, (void *)&mc_ttl, sizeof(mc_ttl))) < 0) {
+        perror("setsockopt(IP_MULTICAST_TTL) failed");
         exit(1);
     }
 
@@ -70,22 +49,16 @@ int main(int argc, char *argv[])
 
     memset(send_str, 0, sizeof(send_str));
 
-    while (fgets(send_str+LWEOFS, MAX_LEN, stdin))
-    {
-        send_len = strlen(send_str+LWEOFS)+LWEOFS;
+    while (fgets(send_str, MaxLen, stdin)) {
+        send_len = strlen(send_str);
 
-#ifdef LWEFORMAT
-        strcpy(send_str, "UdPbC");
-#endif
-
-        /* send string to multicast address */
-        if ((sendto(sock, send_str, send_len, 0, (struct sockaddr *)&mc_addr, sizeof(mc_addr))) != send_len)
-        {
+        // send string to multicast address */
+        if ((sendto(sock, send_str, send_len, 0, (struct sockaddr *)&mc_addr, sizeof(mc_addr))) != send_len){
             perror("sendto() sent incorrect number of bytes");
             exit(1);
         }
 
-        /* clear send buffer */
+        // clear send buffer */
         memset(send_str, 0, sizeof(send_str));
     }
 
